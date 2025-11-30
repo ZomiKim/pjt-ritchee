38925f80e8453169f037da2e73aa6634d0e59662
jhr88 <uchung22@gmail.com>
2025-11-30 16:34:13 +0900

    test

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Tailwind v4 OK</h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Vite + React + Tailwind 4 예제
+      </p>
+    </div>
+  );
+}
+
+export default App;

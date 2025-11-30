ca0988f5b218956fb39697c671727540353acb32
jhr88 <uchung22@gmail.com>
2025-11-30 22:13:28 +0900

    style : settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Tailwind v4 OK</h1>
-      <p className="mt-4 text-lg text-gray-700">
-        Vite + React + Tailwind 4 예제
-      </p>
-    </div>
+    <>
+      <div className="bg-main-01"></div>
+      <div className="bg-main-02">컬러</div>
+      <div className="bg-light-01">컬러</div>
+      <div className="bg-light-02">컬러</div>
+      <div className="bg-deep">컬러</div>
+      <div className="bg-white text-black">컬러</div>
+      <div className="bg-black text-white">컬러</div>
+      <div className="bg-point text-white">컬러</div>
+      <div className="bg-gray-deep text-white">컬러</div>
+      <div className="bg-gray-mid text-white">컬러</div>
+      <div className="bg-gray-light text-black">컬러</div>
+
+      <h1>계절이 지나가는 하늘에는</h1>
+      <h2>계절이 지나가는 하늘에는</h2>
+      <h3>계절이 지나가는 하늘에는</h3>
+      <h4>계절이 지나가는 하늘에는</h4>
+      <span>계절이 지나가는 하늘에는</span>
+      <p>계절이 지나가는 하늘에는</p>
+      <a>계절이 지나가는 하늘에는</a>
+      <span className="dummy block">계절이 지나가는 하늘에는</span>
+    </>
   );
 }
 

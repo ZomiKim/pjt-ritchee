e3129158cd8dbeedc6bedb18a99b052a757f3a02
jhr88 <uchung22@gmail.com>
2025-12-01 23:54:53 +0900

    feat : mypage router settings add

--- src/assets/page/mypage/Mypage.jsx
+++ src/assets/page/mypage/Mypage.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import EditInfo from "./EditInfo";
+import MedicalList from "./MedicalList";
+import ReviewHistory from "./ReviewHistory";
+import ReservationList from "./ReservationList";
+import ReviewForm from "./ReviewForm";
+import DentistReview from "../shared/DentistReview";
 
 function Mypage() {
   return (
     <>
       <div>mypage</div>
+      {/* Inserted because of img url */}
+      <img
+        src="https://ocnuykfvdtebmondqppu.supabase.co/storage/v1/object/public/images/MyPageIMG.png"
+        alt="img"
+      />
+
+      <Routes>
+        <Route path="editInformation" element={<EditInfo />} />
+        <Route path="medicalList" element={<MedicalList />} />
+        <Route path="medicalList/reviewForm/:id" element={<ReviewForm />} />
+        <Route path="reviewHistory" element={<ReviewHistory />} />
+        <Route
+          path="reviewHistory/dentistReview/:id"
+          element={<DentistReview />}
+        />
+        <Route path="reservationList" element={<ReservationList />} />
+      </Routes>
     </>
   );
 }
--- src/assets/page/mypage/ReviewForm.jsx
+++ src/assets/page/mypage/ReviewForm.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ReviewForm() {
   return (
     <>
-      <div>Home</div>
+      <div>ReviewForm</div>
     </>
   );
 }

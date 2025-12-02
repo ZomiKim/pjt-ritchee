0ec7710b5ece05cb1935ab36d568a5c1e55bdf6d
hoons <khg6644089@hanmail.net>
2025-12-02 12:25:01 +0900

    mypage 001

--- src/assets/page/mypage/MedicalList.jsx
+++ src/assets/page/mypage/MedicalList.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { useSearchParams } from "react-router-dom";
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 function MedicalList() {
   const [searchParams] = useSearchParams();
-  const id = searchParams.get("id");
+  const id = searchParams.get('id');
 
   return (
     <>
-      <div>{id ? `${id}번 회원님의 진료 내역` : "진료 내역"}</div>
+      <div>{id ? `${id}번 회원님의 진료 내역` : '진료 내역'}</div>
     </>
   );
 }
--- src/assets/page/mypage/Mypage.jsx
+++ src/assets/page/mypage/Mypage.jsx
@@ -1,21 +1,35 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import EditInfo from "./EditInfo";
-import MedicalList from "./MedicalList";
-import ReviewHistory from "./ReviewHistory";
-import ReservationList from "./ReservationList";
-import ReviewForm from "./ReviewForm";
-import DentistReview from "../shared/DentistReview";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import EditInfo from './EditInfo';
+import MedicalList from './MedicalList';
+import ReviewHistory from './ReviewHistory';
+import ReservationList from './ReservationList';
+import ReviewForm from './ReviewForm';
+import DentistReview from '../shared/DentistReview';
+import { Link } from 'react-router-dom';
 
 function Mypage() {
   return (
     <>
       <div>mypage</div>
-      {/* Inserted because of img url */}
-      <img
-        src="https://ocnuykfvdtebmondqppu.supabase.co/storage/v1/object/public/images/MyPageIMG.png"
-        alt="img"
-      />
+      <div className="wrap !p-0 !m-0">
+        <div className="container !p-0 !m-0">
+          <div
+            className="flex flex-col items-center justify-center text-white !p-0 !m-0 w-full"
+            style={{
+              backgroundImage:
+                'url("https://ocnuykfvdtebmondqppu.supabase.co/storage/v1/object/public/images/MyPageIMG.png")',
+
+              backgroundSize: 'cover',
+
+              width: '100%',
+              height: '300px',
+            }}
+          >
+            <span>김훈규님의 건강을 위한 공간입니다</span>
+          </div>
+        </div>
+      </div>
 
       <Routes>
         <Route path="editInformation" element={<EditInfo />} />

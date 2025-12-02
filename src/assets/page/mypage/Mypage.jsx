1c49e30923c5c43b9c5a2e3c6c6c62b68ce22588
hoons <khg6644089@hanmail.net>
2025-12-02 15:44:19 +0900

    feat : my page

--- src/assets/page/mypage/Mypage.jsx
+++ src/assets/page/mypage/Mypage.jsx
@@ -1,29 +1,63 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import EditInfo from "./EditInfo";
-import MedicalList from "./MedicalList";
-import ReviewHistory from "./ReviewHistory";
-import ReservationList from "./ReservationList";
-import ReviewForm from "./ReviewForm";
-import DentistReview from "../shared/DentistReview";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import EditInfo from './EditInfo';
+import MedicalList from './MedicalList';
+import ReviewHistory from './ReviewHistory';
+import ReservationList from './ReservationList';
+import ReviewForm from './ReviewForm';
+import DentistReview from '../shared/DentistReview';
+import { Link } from 'react-router-dom';
+import Footer from '../../../componetns/footer';
 
 function Mypage() {
   return (
     <>
-      <div>mypage</div>
-
       <div
-        className="flex flex-col items-center justify-center text-white w-full h-[200px] md:h-[300px] lg:h-[400px] myBg"
+        className="flex flex-col items-center justify-center text-white w-full h-[300px] md:h-[400px] lg:h-[500px] myBg"
         style={{
           backgroundImage:
             'url("https://ocnuykfvdtebmondqppu.supabase.co/storage/v1/object/public/images/MyPageIMG.png")',
-          backgroundSize: "cover",
-          backgroundPosition: "center",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
         }}
       >
         <span>김훈규님의 건강을 위한 공간입니다</span>
       </div>
+
+      <div className="wrap">
+        <div className="container">
+          <div className="flex justify-center  gap-3 py-3">
+            <Link
+              to="../mypage/editInformation"
+              className="nav-link px-13 py-2 bg-main-02 text-white rounded-md hover:bg-main-02 "
+            >
+              회원 정보 수정
+            </Link>
+            <Link
+              to="./mypage/EditInfo"
+              className="nav-link px-17 py-2 bg-main-02 text-white rounded-md hover:bg-main-02"
+            >
+              예약 현황
+            </Link>
+          </div>
+          <div className="flex justify-center gap-3">
+            <Link
+              to="./mypage/EditInfo"
+              className="nav-link  px-17 py-2 bg-main-02 text-white rounded-md hover:bg-main-02"
+            >
+              진료 기록
+            </Link>
+            <Link
+              to="./mypage/EditInfo"
+              className="nav-link px-17 py-2 bg-main-02 text-white rounded-md hover:bg-main-02"
+            >
+              후기 기록
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <Footer></Footer>
 
       <Routes>
         <Route path="editInformation" element={<EditInfo />} />

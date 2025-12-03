f7e8594e6a6c66c75930e01200b7a6932b14c773
kartia01 <kartia25142@gmail.com>
2025-12-03 18:24:27 +0900

    event

--- src/assets/page/event/Event.jsx
+++ src/assets/page/event/Event.jsx
@@ -17,47 +17,52 @@ function Event() {
   return (
     <>
       <div>
-        <div onClick={() => selectClick(1)}>
-          <img
-            src="https://uosmaiisnppqgxbcbawc.supabase.co/storage/v1/object/public/images/event1.png"
-            alt="img"
-            className="block sm:hidden w-screen -ml-[50vw] left-1/2 relative"
-          />
-          {/* <img
-            src="https://uosmaiisnppqgxbcbawc.supabase.co/storage/v1/object/public/images/tevent1.jpg"
-            alt="img"
-            className="hidden sm:block lg:hidden w-screen -ml-[50vw] left-1/2 relative"
-          /> */}
+        {/* 이벤트1 */}
+        <div onClick={() => selectClick(1)} >
+          <div>
+            <div className="w-full bg-amber-400 h-[30vh] myBg relative overflow-hidden">
+              <img
+                src="https://uosmaiisnppqgxbcbawc.supabase.co/storage/v1/object/public/images/mevent1.jpg"
+                alt="img"
+                className="block sm:hidden w-full h-full object-cover object-center"
+              />
+            </div>
+          </div>
           <img
             src="https://uosmaiisnppqgxbcbawc.supabase.co/storage/v1/object/public/images/pevent1.jpg"
             alt="img"
-            className="hidden sm:block w-screen relative left-1/2 -translate-x-1/2"
+            className="hidden sm:block myBg"
           />
         </div>
 
+
         <div className="container">
-          <div className="flex items-center gap-[5px] mt-[30px] mb-5 md:my-10">
+          {/* mt-[30px] -> mt-[2.87vh] */}
+          <div className="flex items-center gap-[1.28vw] mt-[2.87vh] mb-5 ">
             <i className="fa-solid fa-tooth md:text-2xl lg:text-4xl xl:text-5xl text-deep"></i>
-            <h4 className="font-Pretendard text-deep" style={{fontSize:"clamp(16px, 4vw, 48px)"}}>구로구 주민 이벤트</h4>
+            <h4 className="font-Pretendard text-deep" style={{ fontSize: 'clamp(16px, 4vw, 40px)' }}>
+              구로구 주민 이벤트
+            </h4>
           </div>
 
-          <p className="mb-[30px] font-Pretendard md:text-[100px]" style={{fontSize:"clamp(10px, 3vw, 36px)"}}>
+          <p className="mb-[2.87vh] font-Pretendard" style={{ fontSize: 'clamp(12px, 3vw, 30px)' }}>
             구로구 주민 여러분을 위한 특별 치과 이벤트! 건강한 치아, 밝은 미소를 위한 무료 검진 및 스케일링 혜택을
-            만나보세요.<br />예약은 선착순! 지금 바로 신청하세요.
+            만나보세요. 예약은 선착순! 지금 바로 신청하세요.
           </p>
         </div>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col myBg">
           <div onClick={() => selectClick(2)}>
             <img
               src="https://uosmaiisnppqgxbcbawc.supabase.co/storage/v1/object/public/images/event2.png"
               alt="img"
-              className="block sm:hidden myBg mb-5"
+              className="block sm:hidden w-full h-full object-cover object-center mb-5"
             />
+
             <img
               src="https://uosmaiisnppqgxbcbawc.supabase.co/storage/v1/object/public/images/pevent2.jpg"
               alt="img"
-              className="hidden sm:block myBg mb-10"
+              className="hidden sm:block mb-10"
             />
           </div>
 
@@ -65,13 +70,13 @@ function Event() {
             <img
               src="https://uosmaiisnppqgxbcbawc.supabase.co/storage/v1/object/public/images/event3.png"
               alt="img"
-              className="block sm:hidden myBg mb-5"
+              className="block sm:hidden w-full h-full object-cover object-center mb-5"
             />
 
             <img
               src="https://uosmaiisnppqgxbcbawc.supabase.co/storage/v1/object/public/images/pevent3.jpg"
               alt="img"
-              className="hidden sm:block myBg mb-10"
+              className="hidden sm:block mb-10"
             />
           </div>
 
@@ -79,12 +84,13 @@ function Event() {
             <img
               src="https://uosmaiisnppqgxbcbawc.supabase.co/storage/v1/object/public/images/event4.png"
               alt="img"
-              className="block sm:hidden myBg mb-5"
+              className="block sm:hidden w-full h-full object-cover object-center mb-5"
             />
+
             <img
               src="https://uosmaiisnppqgxbcbawc.supabase.co/storage/v1/object/public/images/pevent4.jpg"
               alt="img"
-              className="hidden sm:block myBg mb-10"
+              className="hidden sm:block mb-10"
             />
           </div>
         </div>

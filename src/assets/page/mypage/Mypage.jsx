7b3eece6c4531dcfa1fff2ee0adbaa76fa66c365
jhr88 <uchung22@gmail.com>
2025-12-06 22:20:33 +0900

    fix : debug

--- src/assets/page/mypage/Mypage.jsx
+++ src/assets/page/mypage/Mypage.jsx
@@ -39,7 +39,7 @@ function MypageMain() {
               to="/mypage/reservationList"
               className="nav-link px-10 py-2 bg-main-02 text-white rounded-md hover:bg-main-02"
             >
-              예약 현황 예약 현황
+              예약 현황
             </Link>
           </div>
           <div className="flex justify-center gap-3 mb-[100px]">
